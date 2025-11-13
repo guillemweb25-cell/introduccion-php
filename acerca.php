490346caa4129eba9ec26a4c6efcc22ffc7dc707
Tu Nombre <you@example.com>
2025-11-13 11:15:37 +0100

    introducción a php

---
+++ acerca.php
@@ -0,0 +1,7 @@
+<?php
+$pageTitle = "Inicio";
+require __DIR__ . "/includes/header.php";
+?>
+    <h1>Acerca de...</h1>
+<?php
+require __DIR__ . "/includes/footer.php";
---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+$pageTitle = "Inicio";
+require __DIR__ . "/includes/header.php";
+?>
+    <h1>
+    Bienvenido a mi sitio web
+    </h1>
+    
+<?php
+require __DIR__ . "/includes/footer.php";
